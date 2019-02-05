e9ec56f2097b0cfd0a1e2fca6b21e6fd8642c7c3
Laxmi Thoodi <laxmithoodi@zipcoders-MacBook-Pro-3.local>
2019-02-05 17:35:01 -0500

    lab completed test pass

--- Main.java
+++ Main.java
@@ -1,9 +1,21 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
+ 
+Scanner read =  new Scanner (System.in);
+System.out.println ("Enter your name:" );
 
-    }
+    String x = read.nextLine();
+    if (((x.equals ("Alice")) || (x.equals ("Bob")))){
+    
+System.out.println("Hello my name " + x );
+
+}
+else 
+System.out.println("I'm not Bob or Alice" );
+  }  
 }
